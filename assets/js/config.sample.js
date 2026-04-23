636524a95abf475bce6e99971e6e04a81f05b95b
akmalhafizin <akmal.jusoh@lapasar.com>
2026-04-23 18:29:33 +0800

    fix deployment error

--- assets/js/config.sample.js
+++ assets/js/config.sample.js
@@ -1,5 +1,6 @@
 const CONFIG = (function() {
+    const ROOT = ''; // Set to '' for root deployment, or '/subdirectory' if hosted in a subdirectory
     const SUPABASE_URL = "https://aihjbacockwdijgjeoog.supabase.co";
     const SUPABASE_ANON_KEY = "sb_publishable_xGFMkrKSuS4KgJP_RrcqFw_kbVrhHNU";
-    return { SUPABASE_URL, SUPABASE_ANON_KEY };
+    return { ROOT, SUPABASE_URL, SUPABASE_ANON_KEY };
 })();
